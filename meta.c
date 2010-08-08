da43c324f2478ddf459556970d68fad6d81af547
Nikita Danilov <danilov@gmail.com>
2010-08-08 13:27:00 +0400

    add meta-relation.

---
+++ meta.c
@@ -0,0 +1,28 @@
+/* -*- C -*- */
+/* This file is part of R. See the end of the file for the prominent
+   copyright and licensing information. */
+
+#include "meta.h"
+#include "epsilon.h"
+
+struct r_eps_rel r_meta_rel;
+
+void r_meta_init(void)
+{
+	r_eps_rel_init(&r_meta_rel);
+}
+
+void r_meta_fini(void)
+{
+	r_eps_rel_fini(&r_meta_rel);
+}
+
+/* 
+ *  Local variables:
+ *  c-indentation-style: "K&R"
+ *  c-basic-offset: 8
+ *  tab-width: 8
+ *  fill-column: 80
+ *  scroll-step: 1
+ *  End:
+ */
---
+++ meta.h
@@ -0,0 +1,26 @@
+/* -*- C -*- */
+/* This file is part of R. See the end of the file for the prominent
+   copyright and licensing information. */
+
+#ifndef __R_META_H__
+#define __R_META_H__
+
+#include "epsilon.h"
+
+extern struct r_eps_rel r_meta_rel;
+
+void r_meta_init(void);
+void r_meta_fini(void);
+
+/* __R_META_H__ */
+#endif
+
+/* 
+ *  Local variables:
+ *  c-indentation-style: "K&R"
+ *  c-basic-offset: 8
+ *  tab-width: 8
+ *  fill-column: 80
+ *  scroll-step: 1
+ *  End:
+ */
