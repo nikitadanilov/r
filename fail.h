50c5b418677ad3c29111b3200c0a8bbff469e39f
Nikita Danilov <danilov@gmail.com>
2010-08-09 18:36:35 +0400

    add --disable-debug configure option to disable R_{ASSERT,PRE,POST,INVARIANT} macros.

--- fail.h
+++ fail.h
@@ -5,12 +5,22 @@
 #ifndef __R_FAIL_H__
 #define __R_FAIL_H__
 
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
 void r_panic(const char *fmt, 
 	     const char *file, int lineno, const char *func, ...) 
 	__attribute__((noreturn, format(printf, 1, 5)));
 
 #define R_PANIC(str) r_panic(str, __FILE__, __LINE__, __func__)
-#define R_FAIL(cond, str) ((cond) ? ((void)0) : R_PANIC(str))
+
+#ifdef ENABLE_DEBUG
+# define R_FAIL(cond, str) ((cond) ? ((void)0) : R_PANIC(str))
+#else
+# define R_FAIL(cond, str) do {;} while (0)
+#endif
+
 #define R_ASSERT(cond) R_FAIL(cond, "Assertion " #cond " failed")
 #define R_PRE(cond) R_FAIL(cond, "Pre-condition " #cond " failed")
 #define R_POST(cond) R_FAIL(cond, "Post-condition " #cond " failed")
